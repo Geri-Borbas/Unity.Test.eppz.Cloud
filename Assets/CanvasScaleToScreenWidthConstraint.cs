5d5f7ffff6503d24cd4e9fe367d7419a018b14eb
eppz <hello@eppz.eu>
2017-12-15 03:05:44 +0100

    0.0.7

---
+++ Assets/CanvasScaleToScreenWidthConstraint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode]
+public class CanvasScaleToScreenWidthConstraint : MonoBehaviour
+{
+
+
+	public AnimationCurve canvasScaleToScreenWidth;
+	
+	
+	void Update()
+	{
+		GetComponent<CanvasScaler>().scaleFactor =
+			canvasScaleToScreenWidth.Evaluate(Screen.width);
+	}
+}
